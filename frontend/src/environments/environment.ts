2995062bfcb93582bfd9eaf34f886ca8acfd2a9b
franc <onyishif@gmail.com>
2022-07-21 23:38:42 +0100

    add environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'identeefy.eu', // the auth0 domain prefix
+    audience: 'identeefy', // the audience set for the auth0 app
+    clientId: 'NAOhedHox4CpCTP0Quyg7yNJO9DGzp6W', // the client id generated for the auth0 app
+    callbackURL: 'https://localhost:8100', // the base url of the running ionic application. 
   }
 };
